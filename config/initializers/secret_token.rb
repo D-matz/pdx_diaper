352b0a9a13820737a82f6ceb1a630020c2508a1c
Aaron Hill <armahillo@gmail.com>
2016-06-16 23:51:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdxDb::Application.config.secret_token = '93d2290976f07c6eedde7b13da1bd795a7dc8452e7b9c1c20bd1f51376c225c30753dde677972fe1abeae0d14a3fa22482cce4e2bb1ece8c6715fff4f2515a33'
